96390b1ed934bfe71cd6152fa7db922a8a1f9256
gujie <704572066@qq.com>
2024-04-23 10:49:35 +0800

    初始提交

--- src/fields.ts
+++ src/fields.ts
@@ -1,46 +1,49 @@
-import {
-  createTextField,
-  createUrlField,
-  createNumberField,
-  createAvatarField
-} from 'tushan';
+import { createTextField, createNumberField } from 'tushan';
 
 export const userFields = [
   createTextField('id', { label: 'ID' }),
-  createTextField('username', { label: '用户名', list: { sort: true } }),
-  createTextField('password', { label: '密码（加密）' }),
-  createNumberField('balance', { label: '余额' }),
-  createTextField('openaiKey', { label: 'OpenAI Key' }),
-  createTextField('createTime', { label: 'Create Time' }),
-  createAvatarField('avatar', { label: 'Avatar' }),
+  createTextField('username', { label: '用户名' }),
+  createNumberField('balance', { label: '余额（元）', list: { sort: true } }),
+  createTextField('createTime', {
+    label: '创建时间',
+    list: { sort: true },
+    edit: { hidden: true }
+  }),
+  createTextField('password', { label: '密码', list: { hidden: true } })
 ];
 
 export const payFields = [
   createTextField('id', { label: 'ID' }),
-  createTextField('name', { label: '用户名', list: { sort: true } }),
+  createTextField('userId', { label: '用户Id' }),
   createNumberField('price', { label: '支付金额' }),
   createTextField('orderId', { label: 'orderId' }),
   createTextField('status', { label: '状态' }),
-  createTextField('createTime', { label: 'Create Time' }),
+  createTextField('createTime', { label: '创建时间', list: { sort: true } })
 ];
 
 export const kbFields = [
   createTextField('id', { label: 'ID' }),
-  createTextField('user', { label: '所属用户' }),
-  createTextField('name', { label: '知识库', list: { sort: true } }),
-  createTextField('tags', { label: 'Tags' }),
-  createAvatarField('avatar', { label: 'Avatar' }),
+  createTextField('userId', { label: '所属用户', edit: { hidden: true } }),
+  createTextField('name', { label: '知识库' }),
+  createTextField('tags', { label: 'Tags' })
 ];
 
-export const ModelFields = [
+export const AppFields = [
   createTextField('id', { label: 'ID' }),
-  createTextField('name', { label: 'Ai助手', list: { sort: true } }),
-  createTextField('user', { label: '所属用户' }),
-  createTextField('relatedKbs', { label: '引用的知识库' }),
-  createTextField('searchMode', { label: '搜索模式' }),
-  createTextField('systemPrompt', { label: '提示词' }),
+  createTextField('userId', { label: '所属用户', list: { hidden: true }, edit: { hidden: true } }),
+  createTextField('name', { label: '名字' }),
+  createTextField('app', { label: '应用', edit: { hidden: true } }),
+  createTextField('share.collection', { label: '收藏数', list: { sort: true } }),
+  createTextField('share.topNum', { label: '置顶等级', list: { sort: true } }),
+  createTextField('share.isShare', { label: '是否分享(true,false)' }),
+  createTextField('intro', { label: '介绍', list: { width: 400 } }),
+  createTextField('relatedKbs', { label: '引用的知识库', list: { hidden: true } }),
   createTextField('temperature', { label: '温度' }),
-  createTextField('isShare', { label: '是否分享' }),
-  createTextField('isShareDetail', { label: '分享详情' }),
-  createAvatarField('avatar', { label: 'Avatar' }),
+  createTextField('systemPrompt', {
+    label: '提示词',
+    list: {
+      width: 400,
+      hidden: true
+    }
+  })
 ];
